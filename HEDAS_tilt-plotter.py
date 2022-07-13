c667b0490cb82241f7fadf080ae5f54d8d4bffd4
Bruno Rojas <bsr5234@psu.edu>
2022-07-13 17:10:26 -0400

    Changed tilt plotter to use lat/lon for axes and added correct radius for range rings, extent might need more work

--- HEDAS_tilt-plotter.py
+++ HEDAS_tilt-plotter.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import datetime as dt
 from HEDAS_pkg import HEDASds
+import cartopy.crs as ccrs
 
 fname="stored_centers_Dorian.npy" 
 
@@ -36,25 +37,37 @@ for findex, hfile in enumerate(sorted(glob.glob("*.nc"))):
 
     ds=HEDASds(hfile) #read in the the netCDF file
     ds.center_relative_winds(ctr) #calculate wind fields
-    # lat=ds.get_vbl('NLAT_surface')[0]
-    # lon=ds.get_vbl('ELON_surface')[0]
-    plt.contour(ds.radius,levels=[25,50],colors='gray')
+    lat=ds.get_vbl('NLAT_surface')[0]
+    lon=ds.get_vbl('ELON_surface')[0]
+    fig=plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(1,1,1,projection=ccrs.PlateCarree())
+    # plt.contour(ds.radius,levels=[25,50],colors='gray')
+    plt.contour(lon,lat,ds.radius,transform=ccrs.PlateCarree(),levels=[5,10,15,20,25],colors='gray')
+
     # PT=plt.scatter(adjcxs,adjcys,c=stacklvs,cmap=cm.jet)
     # plt.plot(adjcxs,adjcys,'k')
-    PT=plt.scatter(cxsi,cysi,c=stacklvs,cmap=cm.jet)
-    plt.plot(cxsi,cysi,'k')
+
+    #convert the centers from grid coords to lat/lon
+    ctr_lon=lon[ctr[1],ctr[0]]
+    ctr_lat=lat[ctr[1],ctr[0]]
+    cxsi_lon=lon[cysi,cxsi]
+    cysi_lat=lat[cysi,cxsi]
+    # print(cysi_lat,cxsi_lon)
+    PT=ax.scatter(cxsi_lon,cysi_lat,transform=ccrs.PlateCarree(),c=stacklvs,cmap=cm.jet)
+    ax.plot(cxsi_lon,cysi_lat,'k',transform=ccrs.PlateCarree())
     cb1=plt.colorbar(PT)
     cb1.set_label('mb')
-    plotradius=50 #50 grid boxes
-    # plt.xlim(ctr[0]-plotradius,ctr[0]+plotradius)
-    # plt.ylim(ctr[1]-plotradius,ctr[1]+plotradius)
-    plt.xlim(ctr[0]-plotradius,ctr[0]+plotradius)
-    plt.ylim(ctr[1]-plotradius,ctr[1]+plotradius)
+    ax.gridlines(draw_labels=True)
+
+    plotradius=25 # km
+    lonradius=plotradius/111.321 * np.cos(np.deg2rad(ctr_lat))
+    latradius=plotradius/111.321
+
+    ax.set_extent([ctr_lon-lonradius, ctr_lon+lonradius, ctr_lat-latradius,ctr_lat+latradius], crs=ccrs.PlateCarree())
+
     plt.title(f'Vortex centers by pressure level\n{ds.getime()}')
 
-    # plt.show()
     print(ds.getime())
-    os.chdir(srcpath)
+    os.chdir(srcpath+'/tilt_images_zoomed')
     plt.savefig("tilt_%s.png"%ds.datetimestring, bbox_inches="tight") #, dpi=400
     plt.clf()
-
